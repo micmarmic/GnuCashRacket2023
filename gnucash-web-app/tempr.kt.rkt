e1f9c292c48f8bf51f3b73b439b7851acbac7485
micmarmic <mobimich@gmail.com>
2023-11-05 14:36:16 -0500

    trans-in-account->ledger-lines works, ready to integrate into view

--- gnucash-web-app/tempr.kt.rkt
+++ gnucash-web-app/tempr.kt.rkt
@@ -18,38 +18,66 @@
     (if (null? lines)
         result
         (loop (rest lines)
-              (string-append result "\n   " (ledger-line->string (car lines)))))))
+              (string-append result (ledger-line->string (car lines)) "\n")))))
 
 (define (ledger-line->string line)
-  (format "dt:~a desc:~a act:~a val:~a tot:~a bal:~a~%"
-          (ledger-line-date line)
-           (ledger-line-description line)
-           (ledger-line-account-name line)
-           (ledger-line-value line)
-           (ledger-line-total line)
-           (ledger-line-balance line)))
-           
+  ;(format "dt:~a desc:~a act:~a val:~a tot:~a bal:~a"
+  (format "~a ~a ~a ~a ~a ~a"
+          (~a (ledger-line-date line) #:width 12)
+          (~a (ledger-line-description line) #:width 30)
+          (~a (ledger-line-account-name line) #:width 30)
+          (~a (ledger-line-value line) #:width 8)
+          (~a (ledger-line-total line) #:width 8)
+          (~a (ledger-line-balance line) #:width 10)))
 
-; splits formatted for display and transaction value for current account
-(define (compile-all-trans transactions arg-account-id)
-  (struct compiled-splits (display-values transaction-value))
-  (for ([trans transactions])
-    ;(printf "~a~%" (send trans get-date-posted))
-    (let ([compiled 
-           (let loop-splits ([splits (send trans get-splits)] [display-values '()] [total 0])
-             (if (null? splits)
-                 (compiled-splits display-values total)
-                 (let* ([split (car splits)]
-                        [line
-                         (ledger-line "" (send split get-memo)
-                                      (send split get-account-name)
-                                      (real->decimal-string (send split get-value))
-                                      "" "")]
-                        [new-total (if (equal? arg-account-id (send split get-account-id))
+
+;; given splits, build list of ledge-line and return total for transation in current account
+;; return a struct compiled-splits
+(struct compiled-splits (display-values transaction-value))
+(define (compile-splits arg-splits arg-account-id)
+  (let loop-splits ([splits arg-splits] [display-values '()] [total 0])
+    (if (null? splits)
+        ; RETURN VALUE 
+        (compiled-splits display-values total)
+        (let* ([split (car splits)])
+          (if (equal? (send split get-account-id) arg-account-id)
+              ; outcome: don't add split for current account
+              ; call loop with next split and current values
+              (loop-splits (rest splits) display-values (send split get-value))
+              ; outcome: add split to other account
+              (let ([line
+                     (ledger-line "" (send split get-memo)
+                                  (send split get-account-name)
+                                  (real->decimal-string (send split get-value))
+                                  "" "")]
+                    [new-total (if (equal? arg-account-id (send split get-account-id))
                                    (send split get-value)
                                    total)])
-                   (loop-splits (rest splits) (append  display-values (list line)) new-total))))])
-             (printf "Tot:~a Vals:~a~%" (real->decimal-string (compiled-splits-transaction-value compiled))
-                                         (list-ledger-lines->string (compiled-splits-display-values compiled))))))
-
-(compile-all-trans transactions (send account get-id))
+                (loop-splits (rest splits) (append  display-values (list line)) new-total)))))))
+    
+;; compile a list of ledger-lines for trans and splits in given account-id
+(define (trans-in-account->ledger-lines gnucash-data arg-account-id)
+  (let* ([account (send gnucash-data account-by-id arg-account-id)]
+         [transactions (send account transactions-sorted-by-date)])           
+  (let loop ([all-trans transactions] [list-lines '()] [balance 0])
+    (if (null? all-trans)
+        list-lines
+        (let* ([trans (car all-trans)]
+               [compiled (compile-splits (send trans get-splits) arg-account-id)]
+               [total (compiled-splits-transaction-value compiled)]
+               [new-balance (+ total balance)]               
+               [trans-line
+                 (ledger-line (send trans get-date-posted)
+                              (send trans get-description)
+                              "  "
+                              "  "
+                              (real->decimal-string total)
+                              (real->decimal-string new-balance))])
+          (loop (rest all-trans)
+                (append list-lines (list trans-line)
+                                         (compiled-splits-display-values compiled))
+                new-balance))))))
+                                        
+      
+(display (list-ledger-lines->string
+          (trans-in-account->ledger-lines gnucash-data (send account get-id))))
