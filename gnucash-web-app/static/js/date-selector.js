78ff87e07b7d204f389025563c28d3c8b542b878
micmarmic <mobimich@gmail.com>
2023-11-22 16:49:38 -0500

    ROI Report: date is auto set to last day of month

--- gnucash-web-app/static/js/date-selector.js
+++ gnucash-web-app/static/js/date-selector.js
@@ -5,12 +5,9 @@ function goToDate() {
     var host = slashes.concat(window.location.host)
     const base_url = @|form-url|;
     
-    var y = document.getElementById("inputYear");
-    var year = y.value;
-    var m = document.getElementById("inputMonth");
-    var month = m.value;
-    var d = document.getElementById("inputDay");
-    var day = d.value;
+    var year = document.getElementById("inputYear").value;
+    var month = document.getElementById("inputMonth").value;
+    var day = document.getElementById("inputDay").value;
     
     var newURL = `${host}${base_url}${year}-${pad(month, 2)}-${pad(day, 2)}`;
     alert(newURL);
@@ -21,4 +18,32 @@ function pad(num, size) {
     num = num.toString();
     while (num.length < size) num = "0" + num;
     return num;
+}
+
+function resetDays() {
+    var select = document.getElementById('inputDay');
+    var options = select.getElementsByTagName('option');
+    /* remove all days */    
+    for (var i=options.length; i--;) {
+        select.removeChild(options[i]);
+    }
+
+    var year = document.getElementById("inputYear").value;
+    var month = document.getElementById("inputMonth").value;
+    var dt = new Date(year, month, 0); // day 0 == last day prev month
+    var numDays = dt.getDate(); // getDay is day of week!!!
+
+    alert(numDays);
+
+    /* add days */
+    for (var i = 0; i<numDays; i++){
+        var opt = document.createElement('option');
+        var day = i + 1;
+        opt.value = day;
+        opt.innerHTML = day;
+        if(day ===  numDays) {
+            opt.selected = true
+        }
+        select.appendChild(opt);        
+    }
 }
