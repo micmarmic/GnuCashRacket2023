c875b5487f5e4d2555eaa5f9439e91e47907a592
micmarmic <mobimich@gmail.com>
2024-01-18 12:02:06 -0500

    Working on reading apnea data file.

--- lib/string-utils.rkt
+++ lib/string-utils.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(provide wrap-text)
+(require rackunit)
+
+(provide wrap-text list-subset)
 
 ;; take text (string) and return a string
 ;; no longer that max-len wrapped with \n before line-len
@@ -29,3 +31,26 @@
                    [else (loop (rest remaining-words)
                                (append-with-space buffer current-word)
                                result)]))])))
+
+
+;; not as string util but ...
+;; given a list, return a list with the n items from the list starting as pos
+(define (list-subset lst start-pos num-items)
+  (define len (length lst))
+  (when (< start-pos  0)
+    (error "list-subset: cannot start at less than zero"))
+  (when (> start-pos (sub1 len))
+    (error "list-subset: given start-pos past end of list"))
+  ; normalize: if num-items is too large, give all items you can, but no error
+  (define pos (min start-pos (- len start-pos)))
+  (let loop ([pos start-pos]
+             [num num-items]
+             [sublst '()]) ; items added in front, reverse when done
+    (cond [(or (= pos len) (zero? num))
+           (reverse sublst)]
+          [else
+           (loop (add1 pos) (sub1 num) (cons (list-ref lst pos) sublst))])))
+
+(check-equal? (list-subset '(0 1 2 3 4 5) 0 0) '())
+(check-equal? (list-subset '(0 1 2 3 4 5) 1 2) '(1 2))
+(check-equal? (list-subset '(0 1 2 3 4 5) 5 1) '(5))
